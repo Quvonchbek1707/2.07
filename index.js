03077a4a84377f10061dd6db0e4edcc93c45da6b
Quvonchbek1707 <106889398+Quvonchbek1707@users.noreply.github.com>
2025-12-17 13:21:31 +0500

    Remove StudentRouter from index.js
    Removed StudentRouter from the application.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import  express from "express"
-import {PostsRouter, UsersRouter, StudentRouter} from "./src/router/router.js"
+import {PostsRouter, UsersRouter} from "./src/router/router.js"
 
 const app = express()
 app.use(express.json())
 app.use(PostsRouter)
-app.use(StudentRouter)
 app.use(UsersRouter)
 
 
